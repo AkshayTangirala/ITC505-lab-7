c4820265c7f927980e2a9f7601de11cbeba50722
AkshayTangirala <91775329+AkshayTangirala@users.noreply.github.com>
2024-04-15 23:42:50 -0700

    Update index.js

--- index.js
+++ index.js
@@ -17,8 +17,7 @@ app.get('/cs212/homework/8', (req, res) => {
 // Your POST route for /cs212/homework/8
 app.post('/cs212/homework/8', (req, res) => {
     const { adjective, pluralNoun, personName, verb, noun } = req.body;
-    const madLib = `Today, I saw a ${adjective} ${pluralNoun} which made me think of ${personName}. 
-                    Then I decided to ${verb} by the ${noun}.`;
+    const madLib = 'In a whimsical land filled with colorful $(plural noun), where $(adjective) $(noun) roam freely and enchanted $(noun) whisper secrets in the breeze, a brave $(adjective) $(noun) embarked on a daring quest to recover the lost $(noun), guided by ancient $(adjective) maps and fueled by boundless emotion.';
 
     res.send(madLib);
 });
